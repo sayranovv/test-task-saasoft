e0c6982b173c11ac9d32e3549419b57186507f64
sayranovv <sayranoveldar@gmail.com>
2025-08-04 22:09:12 +0500

    Connect store & create adding new account feature

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,28 @@
 import { Button } from '@/components/ui/button'
 import AccountList from '@/components/AccountList.vue'
 import { Plus, CircleQuestionMark } from 'lucide-vue-next'
+import { useMainStore } from '@/stores/main.ts'
+
+const store = useMainStore()
 </script>
 
 <template>
   <div class="max-w-[1200px] mx-auto p-5">
     <header class="flex gap-4 items-center">
       <h1 class="text-2xl font-bold">Учетные записи</h1>
-      <Button class="h-14 w-14" variant="outline">
+      <Button
+        class="h-14 w-14"
+        variant="outline"
+        @click="
+          store.addAccount({
+            id: Date.now(),
+            tags: [{ text: '' }],
+            type: null,
+            login: '',
+            password: '',
+          })
+        "
+      >
         <Plus />
       </Button>
     </header>
